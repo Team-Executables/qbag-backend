81fdc6652d51f1c099f4dff25268139570492392
Umang_Thadani <thadaniumang@outlook.com>
2022-03-23 10:39:28 +0530

    Added questions url

--- qbag_api/urls.py
+++ qbag_api/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('questions/', include('questions.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

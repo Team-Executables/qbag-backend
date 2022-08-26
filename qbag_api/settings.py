895ed6755c153b6247435e7d1fff549d2125fb3f
Naman Shah <nashah2001@gmail.com>
2022-08-26 16:17:19 +0530

    db changed to heroku postgres

--- qbag_api/settings.py
+++ qbag_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 import datetime
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,12 +96,9 @@ WSGI_APPLICATION = 'qbag_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config()
 
 # authentication settings
 
